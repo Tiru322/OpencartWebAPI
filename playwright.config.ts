ecec1b2b6fd7c78d729a13b98cf0a18df511ff74
Tiru322 <tirumalababu.sandu@gmail.com>
2026-09-11 15:50:04 +0530

    adding pw config details

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? '50%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
@@ -28,7 +28,7 @@ export default defineConfig({
     video:"retain-on-failure",
    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:false,
+    headless:true,
   },
 
   /* Configure projects for major browsers */
