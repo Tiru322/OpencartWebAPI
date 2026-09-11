8bbbb28bc5d27537b9280c698ce4d133ab1c4601
Tiru322 <tirumalababu.sandu@gmail.com>
2026-09-11 16:30:57 +0530

    add headless browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     video:"retain-on-failure",
    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:true,
+    headless:!process.env.CI?false:true,
   },
 
   /* Configure projects for major browsers */
